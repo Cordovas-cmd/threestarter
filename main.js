2a45c53ca39628d23d42da5daf18351d8f930d35
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-10-24 15:04:56 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+
+// To actually be able to display anything with three.js, we need three things: scene, camera and renderer, so that we can render the scene with camera.
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+const renderer = new THREE.WebGLRenderList();
+renderer.setSize( window.innerWidth, window.innerHeight);
+document.body.appendChild( renderer.domElement); 
